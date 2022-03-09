dddd89257cd2ad21ea12d9d80bf90c3bd952f740
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-03-09 10:50:41 +0000

    header

--- src/components/Header.client.jsx
+++ src/components/Header.client.jsx
@@ -1,55 +1,72 @@
 import {useState} from 'react';
-import {Link} from '@shopify/hydrogen/client';
+import {Image, Link} from '@shopify/hydrogen/client';
 import {ImLocation, ImPhone} from 'react-icons/im';
+import {BiSearchAlt} from 'react-icons/bi';
 
 import CartToggle from './CartToggle.client';
-import CurrencySelector from './CurrencySelector.client';
-import Navigation from './Navigation.client';
 import MobileNavigation from './MobileNavigation.client';
 
 /**
  * A client component that specifies the content of the header on the website
  */
-export default function Header({collections, storeName}) {
+export default function Header({collections}) {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [query, setQuery] = useState('');
 
   return (
-    <header className="h-20 lg:h-32" role="banner">
+    <header role="banner">
       <div
-        className={`fixed z-20 h-20 lg:h-32 w-full border-b border-gray-200 px-6 md:px-8 md:py-6 lg:pt-8 lg:pb-0 mx-auto bg-white ${
+        className={`fixed z-20 w-full border-b border-gray-200 px-6 md:px-0 md:py-0 lg:pb-0 mx-auto bg-white ${
           isMobileNavOpen ? '' : 'bg-opacity-95'
         }`}
       >
-        <div className="flex flex-row items-center justify-between">
-          <div className="flex flex-row items-center space-x-4">
-            <p className="text-appBlack text-[16px]">Terms and Conditions</p>
-            <p className="text-appBlack text-[16px]">Privacy Policy</p>
+        <div className="flex flex-row items-center justify-between h-[41px] px-[132px]">
+          <div className="flex flex-row items-center space-x-8">
+            <p className="text-appBlack text-[14px]">Terms and Conditions</p>
+            <p className="text-appBlack text-[14px]">Privacy Policy</p>
           </div>
-          <div className="flex flex-row items-center space-x-4">
+          <div className="flex flex-row items-center space-x-8">
             <span className="flex flex-row items-center space-x-2">
               <ImLocation />
-              <p>3rd Street, Makola - Accra</p>
+              <p className="text-appBlack text-[14px]">
+                3rd Street, Makola - Accra
+              </p>
             </span>
             <span className="flex flex-row items-center space-x-2">
               <ImPhone />
-              <p>0241234567</p>
+              <p className="text-appBlack text-[14px]">0241234567</p>
             </span>
           </div>
         </div>
-        <div className="flex h-full lg:flex-col place-content-between">
-          <div className="flex items-center justify-between w-full text-center">
-            <MobileNavigation
-              collections={collections}
-              isOpen={isMobileNavOpen}
-              setIsOpen={setIsMobileNavOpen}
+        <div className="flex flex-row items-center justify-between bg-appBlack h-[66px] px-[132px]">
+          <Link to="/">
+            <Image src={'/pharma.png'} width={103} height={30} />
+          </Link>
+          <form className="flex flex-row items-center bg-white rounded w-[512px] h-[40px] pl-4 pr-2">
+            <input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              className="w-full bg-transparent outline-none ring-0"
+              placeholder="Search drug"
             />
-            <Link
-              className="text-3xl font-black tracking-widest uppercase"
-              to="/"
-            >
-              {storeName}
-            </Link>
+            <button className="outline-none ring-0">
+              <BiSearchAlt color="#AAAAAA" size={30} />
+            </button>
+          </form>
+          <div className="flex flex-row items-center space-x-8">
+            <CartToggle />
+            <button className="bg-transparent text-white text-[16px] tracking-wider">
+              Sign In
+            </button>
+            <button className="bg-[#15A383] text-white text-[16px] px-4 py-2 tracking-wider rounded">
+              Register
+            </button>
           </div>
+          <MobileNavigation
+            collections={collections}
+            isOpen={isMobileNavOpen}
+            setIsOpen={setIsMobileNavOpen}
+          />
         </div>
       </div>
     </header>
